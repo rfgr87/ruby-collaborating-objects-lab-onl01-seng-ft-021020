9fecd5bd28d7b4af8b1546beb77bdd2c6f5ff682
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-24 21:12:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,8 +2,17 @@ class MP3Importer
   
   def initialize(file)
     @file = file
+    @mp3_file_names = []
   end
   
-  def files
+  def files(file)
+    @mp3_file_names = Dir.entries(file)
+  end
   
+  def import
+    @mp3_file_names.each do |x|
+      Song.new_by_filename(x)
+    end
+  end
+    
 end
