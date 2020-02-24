3a9574c3cea3d2cbd21bae4af34658c7d330e00d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-24 20:37:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,5 +11,8 @@ class Song
   end
   
   def new_by_filename(filename)
-    @song_name = filename.split(" - ")[0]
+    @song_name = filename.split(" - ")[1]
+    @song = Song.new(@song_name)
+    @song.artist = filename.split(" - ")[0]
+    
 end 
