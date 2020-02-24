54ecad58feeed8845b902381316018996eca80b7
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-24 20:43:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,15 @@ class Song
   def new_by_filename(filename)
     @song_name = filename.split(" - ")[1]
     @song = Song.new(@song_name)
-    @song.artist = filename.split(" - ")[0]
+    @artist = 
+    if Artist.all.include?(filename.split(" - ")[0])
+      Artist.all.select{|x| x.name == filename.split(" - ")[0]} 
+    
+    @song.artist = 
+    @song
+  end
+
+  
+  
     
 end 
